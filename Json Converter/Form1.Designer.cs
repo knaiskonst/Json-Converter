f8eb6fa4d16ac62728b7298a81814c9ddcd78242
Konstantinos Knais <konstantinosknais@gmail.com>
2021-05-04 10:50:26 +0300

    Add project files.

---
+++ Json Converter/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace Json_Converter
+{
+    partial class JsonSelector
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnJsonSelect = new System.Windows.Forms.Button();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.txtShow = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnJsonSelect
+            // 
+            this.btnJsonSelect.Location = new System.Drawing.Point(23, 33);
+            this.btnJsonSelect.Name = "btnJsonSelect";
+            this.btnJsonSelect.Size = new System.Drawing.Size(72, 40);
+            this.btnJsonSelect.TabIndex = 0;
+            this.btnJsonSelect.Text = "Select JSON";
+            this.btnJsonSelect.UseVisualStyleBackColor = true;
+            this.btnJsonSelect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(23, 15);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(67, 15);
+            this.lblPath.TabIndex = 1;
+            this.lblPath.Text = "Path To File";
+            // 
+            // txtShow
+            // 
+            this.txtShow.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.txtShow.Location = new System.Drawing.Point(448, 12);
+            this.txtShow.Multiline = true;
+            this.txtShow.Name = "txtShow";
+            this.txtShow.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtShow.Size = new System.Drawing.Size(340, 426);
+            this.txtShow.TabIndex = 2;
+            this.txtShow.TextChanged += new System.EventHandler(this.txtShow_TextChanged);
+            // 
+            // JsonSelector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtShow);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.btnJsonSelect);
+            this.Name = "JsonSelector";
+            this.Text = "JSON";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnJsonSelect;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.TextBox txtShow;
+    }
+}
+
---
+++ Json Converter/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Json_Converter
+{
+    public partial class JsonSelector : Form
+    {
+        string filePath = string.Empty;
+        string fileContent = string.Empty;
+        public JsonSelector()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                //openFileDialog.InitialDirectory = "C:\\Users/%USER%/Desktop";
+                openFileDialog.Filter = "JSON file |*.json";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                        //txtShow.Text = fileContent;
+                    }
+                }
+            }
+            showFilepath();
+            var decodedText = decode();
+            txtShow.Text = decodedText;
+        }
+
+        private void showFilepath()
+        {
+            lblPath.Text = filePath;
+            var filename = Path.GetFileName(filePath);
+            lblPath.Text = filename;
+            return;
+        }
+        
+        private string decode()
+        { //If file contains characters like \u00ce 
+            Encoding targetEncoding = Encoding.GetEncoding("ISO-8859-1");
+            var unescapeText = System.Text.RegularExpressions.Regex.Unescape(fileContent);
+            return Encoding.UTF8.GetString(targetEncoding.GetBytes(unescapeText));
+        }
+
+        private void txtShow_TextChanged(object sender, EventArgs e)
+        {
+            txtShow.ScrollBars = ScrollBars.Vertical;
+            txtShow.WordWrap = false;
+        }
+    }
+
+    public static class Prompt
+    {
+        public static string ShowDialog(string text, string caption)
+        {
+            Form prompt = new Form()
+            {
+                Width = 500,
+                Height = 150,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            Label textLabel = new Label() { Left = 50, Top = 20, Text = text };
+            TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 400 };
+            Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(textBox);
+            prompt.Controls.Add(confirmation);
+            prompt.Controls.Add(textLabel);
+            prompt.AcceptButton = confirmation;
+
+            return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+        }
+    }
+}
